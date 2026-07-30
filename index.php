ac628ed49310214a421469066259a23e835838f8
projectkinda <project.mvp.bluepill@gmail.com>
2026-07-30 16:50:28 +0530

    CRITICAL FIX: 302 Redirect root / to /index.html to completely bypass stale Nginx root directory cache

--- index.php
+++ index.php
@@ -1,8 +1,4 @@
 <?php
-header('Cache-Control: no-store, no-cache, must-revalidate, max-age=0, s-maxage=0, private');
-header('Pragma: no-cache');
-header('Expires: Thu, 01 Jan 1970 00:00:00 GMT');
-header('X-Accel-Expires: 0');
-readfile(__DIR__ . '/index.html');
+header('Location: /index.html', true, 302);
 exit;
 ?>
