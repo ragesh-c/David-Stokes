b4c32637d894a781dac485c5cf0f56ad349af297
projectkinda <project.mvp.bluepill@gmail.com>
2026-07-30 16:43:47 +0530

    CRITICAL CACHE PURGE: Add X-Accel-Expires: 0 and ServiceWorker/Cache Storage auto-purge to force instant update on all devices

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php
-header('Cache-Control: no-cache, no-store, must-revalidate');
+header('Cache-Control: no-store, no-cache, must-revalidate, max-age=0, s-maxage=0, private');
 header('Pragma: no-cache');
-header('Expires: 0');
+header('Expires: Thu, 01 Jan 1970 00:00:00 GMT');
+header('X-Accel-Expires: 0');
 readfile(__DIR__ . '/index.html');
 exit;
 ?>
